ca4e2142c1d0f8734da804a262082ed9dfd8aa46
Scott Sullivan <scottmsul@gmail.com>
2025-07-28 10:53:48 -0400

    added simple ad-hoc example which reproduces the well-known 2 prod/2 quality split with 79.9 normal inputs per legendary output

--- src/solver.js
+++ src/solver.js
@@ -1,12 +1,203 @@
 import GLPK from '../node_modules/glpk.js/dist/index.js';
 
-async function solve() {
+const MAX_QUALITY_UNLOCKED = 4;
+const JUMP_QUALITY_PROBABILITY = 0.1;
+const NUM_MODULE_SLOTS = 4;
+const QUALITY_PROBABILITY = 0.062; // legendary tier-3
+const PROD_BONUS = 0.25; // legendary tier-3
+const INPUT_COST = 1.0;
+const RECIPE_COST = 0.0;
+const OUTPUT_AMOUNT = 1.0;
+const INPUT_ITEM_NAME = 'example_ingredient';
+const OUTPUT_ITEM_NAME = 'example_product';
+const CRAFTING_RECIPE_NAME = 'example_crafting';
+const RECYCLING_RECIPE_NAME = 'example_recycling';
+const ITEM_NAMES = [INPUT_ITEM_NAME, OUTPUT_ITEM_NAME];
+
+function print(res) {
+    const el = window.document.getElementById('out');
+    el.innerHTML = `Solution: LP \n\n ${JSON.stringify(res, null, 2)}`;
+};
+
+function calculate_quality_probability_factor(starting_quality, ending_quality, max_quality_unlocked, quality_percent) {
+    if (starting_quality > max_quality_unlocked) {
+        throw new Error('Starting quality cannot be above max quality unlocked');
+    } else if(ending_quality > max_quality_unlocked) {
+        throw new Error('Ending quality cannot be above max quality unlocked');
+    } else if (ending_quality < starting_quality) {
+        throw new Error('Ending quality cannot be below starting quality');
+    }
+
+    if ((ending_quality == starting_quality) && (starting_quality == max_quality_unlocked)) {
+        // in this case there are no further qualities we can advance to, so quality remains the same with 100% probability.
+        return 1.0;
+    } else if (ending_quality == starting_quality) {
+        // the probability that quality remains the same is (1 - probability-to-advance)
+        return (1.0 - quality_percent);
+    } else if ((ending_quality > starting_quality) && (ending_quality < max_quality_unlocked)) {
+        // in this case we are producing a higher level quality with probability of quality_percent,
+        // and jumped (ending_quality - starting_quality - 1) extra qualities with JUMP_QUALITY_PROBABILITY each time,
+        // and the chance it doesn't advance further is 1-JUMP_QUALITY_PROBABILITY
+        return quality_percent * (1.0-JUMP_QUALITY_PROBABILITY) * JUMP_QUALITY_PROBABILITY**(ending_quality - starting_quality - 1);
+    } else if ((ending_quality > starting_quality) && (ending_quality == max_quality_unlocked)) {
+        // this is the same case as above but without any probability of jumping further
+        return quality_percent * JUMP_QUALITY_PROBABILITY**(ending_quality - starting_quality - 1);
+    } else {
+        throw new Error(`Reached impossible condition in calculate_quality_probability_factor. Starting_quality=${starting_quality}, ending_quality=${ending_quality}, max_quality_unlocked=${max_quality_unlocked}`);
+    }
+}
+
+function get_solver_variable(recipe_name, starting_quality, num_prod_modules, num_quality_modules) {
+    return `recipe=${recipe_name}__starting_quality=${starting_quality}__num_prod_modules=${num_prod_modules}__num_quality_modules=${num_quality_modules}`;
+}
+
+function get_item_variable(item_name, item_quality) {
+    return `item=${item_name}__quality=${item_quality}`;
+}
+
+async function solve_simple_factorio() {
+    // start with simple ad-hoc example, abstract stuff later
+    // one crafting recipe with one ingredient -> one product
     const glpk = await GLPK();
 
-    function print(res) {
-        const el = window.document.getElementById('out');
-        el.innerHTML = `Solution: LP \n\n ${JSON.stringify(res, null, 2)}`;
+    // initialize costs
+    let variable_costs = new Map();
+
+
+    // initialize item-variable connections
+    let item_variables = new Map();
+    for(let item_name of ITEM_NAMES) {
+        for(let quality = 0; quality <= MAX_QUALITY_UNLOCKED; quality++) {
+            let item_variable = get_item_variable(item_name, quality);
+            item_variables.set(item_variable, []);
+        }
+    }
+
+    // initialize item constraints
+    let item_constraints = new Map();
+    for(let item_name of ITEM_NAMES) {
+        for(let quality = 0; quality <= MAX_QUALITY_UNLOCKED; quality++) {
+            let item_variable = get_item_variable(item_name, quality);
+            item_constraints.set(item_variable, 0);
+        }
+    }
+    let output_item = get_item_variable(OUTPUT_ITEM_NAME, 4);
+    item_constraints.set(output_item, OUTPUT_AMOUNT);
+
+    // setup inputs
+    let input_name = get_item_variable(INPUT_ITEM_NAME, 0);
+    let input_variable = `free_input__item=${input_name}`;
+    item_variables.get(input_name).push({
+        name: input_variable,
+        coef: (1.0)*INPUT_COST
+    });
+    variable_costs.set(input_variable, INPUT_COST);
+
+    // setup crafting recipes
+    for(let starting_quality = 0; starting_quality <= MAX_QUALITY_UNLOCKED; starting_quality++) {
+        for(let num_quality_modules = 0; num_quality_modules <= NUM_MODULE_SLOTS; num_quality_modules++) {
+            let num_prod_modules = NUM_MODULE_SLOTS - num_quality_modules;
+            let recipe_name = CRAFTING_RECIPE_NAME;
+            let recipe_variable = get_solver_variable(recipe_name, starting_quality, num_prod_modules, num_quality_modules)
+
+            let ingredient_name = INPUT_ITEM_NAME;
+            let ingredient_variable = get_item_variable(ingredient_name, starting_quality);
+            let ingredient_amount = 1.0;
+            item_variables.get(ingredient_variable).push({
+                name: recipe_variable,
+                coef: (-1.0)*ingredient_amount
+            });
+
+            for(let ending_quality = starting_quality; ending_quality <= MAX_QUALITY_UNLOCKED; ending_quality++) {
+                let product_name = OUTPUT_ITEM_NAME;
+                let product_variable = get_item_variable(product_name, ending_quality);
+                let total_prod_factor = 1.0 + (num_prod_modules * PROD_BONUS);
+                let total_quality_factor = calculate_quality_probability_factor(starting_quality, ending_quality, MAX_QUALITY_UNLOCKED, num_quality_modules*QUALITY_PROBABILITY);
+                let product_amount = 1.0 * total_prod_factor * total_quality_factor;
+                item_variables.get(product_variable).push({
+                    name: recipe_variable,
+                    coef: product_amount
+                });
+            }
+
+            variable_costs.set(recipe_variable, RECIPE_COST);
+        }
+    }
+
+    // setup recycling recipes
+    for(let starting_quality = 0; starting_quality <= MAX_QUALITY_UNLOCKED; starting_quality++) {
+        let num_quality_modules = NUM_MODULE_SLOTS;
+        let num_prod_modules = 0;
+        let recipe_name = RECYCLING_RECIPE_NAME;
+        let recipe_variable = get_solver_variable(recipe_name, starting_quality, num_prod_modules, num_quality_modules)
+
+        let ingredient_name = OUTPUT_ITEM_NAME;
+        let ingredient_variable = get_item_variable(ingredient_name, starting_quality);
+        let ingredient_amount = 1.0;
+        item_variables.get(ingredient_variable).push({
+            name: recipe_variable,
+            coef: (-1.0)*ingredient_amount
+        });
+
+        for(let ending_quality = starting_quality; ending_quality <= MAX_QUALITY_UNLOCKED; ending_quality++) {
+            let product_name = INPUT_ITEM_NAME;
+            let product_variable = get_item_variable(product_name, ending_quality);
+            let total_prod_factor = 1.0 + (num_prod_modules * PROD_BONUS);
+            let total_quality_factor = calculate_quality_probability_factor(starting_quality, ending_quality, MAX_QUALITY_UNLOCKED, num_quality_modules*QUALITY_PROBABILITY);
+            let product_amount = 0.25 * total_prod_factor * total_quality_factor;
+            item_variables.get(product_variable).push({
+                name: recipe_variable,
+                coef: product_amount
+            });
+        }
+    }
+
+    let formatted_variable_costs = [];
+    variable_costs.forEach( (value, key, map) => {
+        formatted_variable_costs.push({
+            name: key,
+            coef: value
+        })
+    });
+
+    let formatted_item_variables = [];
+    item_variables.forEach( (value, key, map) => {
+        formatted_item_variables.push({
+            name: key,
+            vars: value,
+            bnds: {
+                type: glpk.GLP_FX,
+                ub: item_constraints.get(key),
+                lb: item_constraints.get(key)
+            }
+        })
+    });
+
+    const lp = {
+        name: 'LP',
+        objective: {
+            direction: glpk.GLP_MIN,
+            name: 'obj',
+            vars: formatted_variable_costs
+        },
+        subjectTo: formatted_item_variables
+    }
+
+    const opt = {
+        msglev: glpk.GLP_MSG_OFF
     };
+
+    glpk.solve(lp, opt)
+        .then(res => print(res))
+        .catch(err => console.log(err));
+
+    console.log(await glpk.solve(lp, glpk.GLP_MSG_DBG));
+
+    window.document.getElementById('cplex').innerHTML = await glpk.write(lp);
+}
+
+async function solve_glpk_example() {
+    const glpk = await GLPK();
 
     const lp = {
         name: 'LP',
@@ -20,7 +211,7 @@ async function solve() {
         },
         subjectTo: [
             {
-            name: 'cons1',
+                name: 'cons1',
                 vars: [
                     { name: 'x1', coef: 1.0 },
                     { name: 'x2', coef: 2.0 }
@@ -51,4 +242,5 @@ async function solve() {
     window.document.getElementById('cplex').innerHTML = await glpk.write(lp);
 }
 
-window.document.getElementById('solve').onclick = solve;
+//window.document.getElementById('solve').onclick = solve_glpk_example;
+window.document.getElementById('solve').onclick = solve_simple_factorio;
