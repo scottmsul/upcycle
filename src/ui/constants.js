6ab7c7173aa2303334b34b089d9029cab31dbc3b
Scott Sullivan <scottmsul@gmail.com>
2025-08-07 14:38:47 -0400

    added tabs

--- src/ui/constants.js
+++ src/ui/constants.js
@@ -1,3 +1,8 @@
+export const SOLVER_TAB_BUTTON_ID = 'solver-tab-button';
+export const PREFERENCES_TAB_BUTTON_ID = 'preferences-tab-button';
+export const SOLVER_TAB_ID = 'solver-tab';
+export const PREFERENCES_TAB_ID = 'preferences-tab';
+
 export const RESULT_ID = 'result';
 export const INPUT_ITEMS_TABLE_ID = 'input-items';
 export const OUTPUT_ITEMS_TABLE_ID = 'output-items';
--- src/ui/initialize.js
+++ src/ui/initialize.js
@@ -1,7 +1,28 @@
 import { add_input_item, add_output_item } from "./itemTables.js";
+import { SOLVER_TAB_BUTTON_ID, SOLVER_TAB_ID, PREFERENCES_TAB_BUTTON_ID, PREFERENCES_TAB_ID } from "./constants.js";
 
 export function initialize_ui() {
     // run these each once to have some starting items
     add_input_item();
     add_output_item();
+    initialize_tabs();
+}
+
+function initialize_tabs() {
+    let solver_tab = document.getElementById(SOLVER_TAB_ID);
+    let preferences_tab = document.getElementById(PREFERENCES_TAB_ID);
+    let tabs = [solver_tab, preferences_tab];
+
+    let set_tab = (selected_tab_id) => {
+        for(let tab of tabs) {
+            if(tab.id == selected_tab_id) {
+                tab.style.setProperty('display', 'block');
+            } else {
+                tab.style.setProperty('display', 'none');
+            }
+        }
+    };
+
+    document.getElementById(SOLVER_TAB_BUTTON_ID).onclick = () => set_tab(SOLVER_TAB_ID);
+    document.getElementById(PREFERENCES_TAB_BUTTON_ID).onclick = () => set_tab(PREFERENCES_TAB_ID);
 }
